084d419d77e1bf93c27a4cb40236839fc528a5f3
Engin Gokmen <engin.gokmen@icloud.com>
2024-10-20 01:30:34 +0300

    restructures files

--- employee-management/index.js
+++
@@ -1,5 +0,0 @@
-import {EmployeeManagement} from './webComponents/employee-management';
-import {EmployeeRecords} from './webComponents/employee-records';
-import {AddEditEmployee} from './webComponents/add-edit-employee';
-
-export {EmployeeManagement, EmployeeRecords, AddEditEmployee};
