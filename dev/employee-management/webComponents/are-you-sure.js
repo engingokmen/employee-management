a46b29d89546799309b2c387a06b164bdd02fb18
Engin Gokmen <engin.gokmen@icloud.com>
2024-10-22 13:26:49 +0300

    dialog

--- dev/employee-management/webComponents/are-you-sure.js
+++ dev/employee-management/webComponents/are-you-sure.js
@@ -1,8 +1,20 @@
-import {LitElement, html, css} from 'lit';
+import {LitElement, html, css, render} from 'lit';
 
 export class AreYouSure extends LitElement {
   static get styles() {
-    return css``;
+    return css`
+      #dialog-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    `;
   }
 
   static get properties() {
@@ -12,22 +24,62 @@ export class AreYouSure extends LitElement {
     };
   }
 
+  constructor() {
+    super();
+    this.show = false;
+    this.dialog = null;
+    this.action = '';
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('show')) {
+      if (this.show) {
+        this.openDialog();
+      } else {
+        this.closeDialog();
+      }
+    }
+  }
+
+  openDialog() {
+    if (!this.dialog) {
+      this.dialog = document.createElement('div');
+      this.dialog.id = 'dialog-overlay';
+      document.body.appendChild(this.dialog);
+    }
+    this.renderTemplate();
+  }
+
+  closeDialog() {
+    if (this.dialog) {
+      this.dialog.remove();
+      this.dialog = null;
+    }
+  }
+
   handleSubmit() {
     this.dispatchEvent(new CustomEvent('submit'));
+    this.closeDialog();
   }
 
   handleCancel() {
     this.dispatchEvent(new CustomEvent('cancel'));
+    this.closeDialog();
   }
 
-  render() {
-    return this.show
-      ? html`<dialog open>
-          <p>Are you sure to ${this.action.toUpperCase()} ?</p>
-          <button @click=${this.handleSubmit}>Ok</button>
-          <button @click=${this.handleCancel}>Cancel</button>
-        </dialog> `
-      : '';
+  renderTemplate() {
+    render(
+      html`<dialog open>
+        <p>Are you sure to ${this.action.toUpperCase()} ?</p>
+        <button @click=${this.handleSubmit}>Ok</button>
+        <button @click=${this.handleCancel}>Cancel</button>
+      </dialog> `,
+      this.dialog
+    );
+    document.body.appendChild(this.dialog);
   }
 }
 
