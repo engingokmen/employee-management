593d39dcc3e758f68d6a9380810d542d89d5f013
Engin Gokmen <engin.gokmen@icloud.com>
2024-10-22 14:27:19 +0300

    navigation

--- dev/employee-management/webComponents/navigation-c.js
+++ dev/employee-management/webComponents/navigation-c.js
@@ -1,16 +1,52 @@
 import {LitElement, html, css} from 'lit';
+import {router} from '../router';
 
 export class Navigation extends LitElement {
   static get styles() {
-    return css``;
+    return css`
+      ul {
+        list-style-type: none;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+
+        & a {
+          text-decoration: none;
+          color: var(--blue);
+
+          &:hover {
+            color: var(--lightBlue);
+          }
+        }
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      linkHref: Object,
+    };
+  }
+
+  constructor() {
+    super();
+    this.linkHref = {
+      employees: router.location.routes[0].path,
+      addEmployee: router.location.routes[0].children[1].path,
+    };
   }
 
   render() {
     return html`
       <nav>
         <ul>
-          <li><a href="/">Employees</a></li>
-          <li><a href="/add-edit-employee">Add Employee</a></li>
+          <li>
+            <a href=${this.linkHref.employees}>Employees</a>
+          </li>
+          <li>
+            <a href=${this.linkHref.addEmployee}>Add Employee</a>
+          </li>
         </ul>
       </nav>
     `;
